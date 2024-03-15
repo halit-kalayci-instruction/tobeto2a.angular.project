0ce5bf08d110c4693df6c30876922530e64c8233
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-15 14:35:40 +0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './shared/pages/homepage/homepage.component';
+import { BookListComponent } from './features/book/pages/book-list/book-list.component';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'books', component: BookListComponent, canActivate: [authGuard] },
+];
